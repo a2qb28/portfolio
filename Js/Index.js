fb0702c7b728aa78eb61d4d12f6361a1127e655b
a2qb28 <104405235+a2qb28@users.noreply.github.com>
2022-06-13 18:52:20 -0400

    Add files via upload

---
+++ Js/Index.js
@@ -0,0 +1,41 @@
+console.log("Your index.js file is loaded correctly")
+
+ScrollReveal().reveal('.nav-container');
+ScrollReveal().reveal('.name', { delay: 500 });
+ScrollReveal().reveal('.design', { delay: 900  });
+
+window.sr = ScrollReveal();
+sr.reveal(".decorated",{
+    duration:3000
+       
+});
+
+window.sr = ScrollReveal();
+sr.reveal(".card-1",{
+    duration:3000
+
+       
+});
+
+window.sr = ScrollReveal();
+sr.reveal(".card-2",{
+    duration:3000
+        
+           
+});
+
+window.sr = ScrollReveal();
+    sr.reveal(".card-3",{
+        duration:4000
+        
+           
+});
+
+window.sr = ScrollReveal();
+    sr.reveal("card-4",{
+        duration:6000
+           
+});
+
+/* UX / UI DESIGN */
+
